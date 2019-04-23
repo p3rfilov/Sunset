264e35d1a25ec4dd3b562adc4e9dfcef15f9c30b
Andrej Perfilov <andrej.perfilov@touchsurgery.com>
2019-04-23 17:56:02 +0100

    low altitude clouds and snow particles

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class LookAt : MonoBehaviour
+{
+    public Transform target;
+    public bool maintainDistance;
+    public float distance = 10f;
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target);
+            if (maintainDistance)
+            {
+                var _dist = Vector3.Distance(transform.position, target.position);
+                if (_dist < distance)
+                {
+                    transform.position += (transform.position - target.position).normalized * (distance - _dist);
+                }
+            }
+        }
+    }
+}
