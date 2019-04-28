57ffdca41b775c0a44302b9f5861058af32c71f0
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2019-04-28 23:35:37 +0100

    WIP balloon rig/animation

---
+++ Assets/Scripts/SimpleSmoothMouseLook.cs
@@ -0,0 +1,82 @@
+﻿// Sligtly edited version
+// source: https://forum.unity.com/threads/a-free-simple-smooth-mouselook.73117/
+
+using UnityEngine;
+
+public class SimpleSmoothMouseLook : MonoBehaviour
+{
+    Vector2 _mouseAbsolute;
+    Vector2 _smoothMouse;
+
+    [Range(-180f, 0f)] public float minimumX = -180f;
+    [Range(0f, 180f)] public float maximumX = 180f;
+    [Range(-90f, 0f)] public float minimumY = -90f;
+    [Range(0f, 90f)] public float maximumY = 90f;
+
+    public bool lockCursor;
+    public Vector2 sensitivity = new Vector2(2, 2);
+    public Vector2 smoothing = new Vector2(3, 3);
+    public Vector2 targetDirection;
+    public Vector2 targetCharacterDirection;
+
+    // Assign this if there's a parent object controlling motion, such as a Character Controller.
+    // Yaw rotation will affect this object instead of the camera if set.
+    public GameObject characterBody;
+
+    void Start()
+    {
+        // Set target direction to the camera's initial orientation.
+        targetDirection = transform.localRotation.eulerAngles;
+
+        // Set target direction for the character body to its inital state.
+        if (characterBody)
+            targetCharacterDirection = characterBody.transform.localRotation.eulerAngles;
+    }
+
+    void Update()
+    {
+        // Ensure the cursor is always locked when set
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Allow the script to clamp based on a desired target value.
+        var targetOrientation = Quaternion.Euler(targetDirection);
+        var targetCharacterOrientation = Quaternion.Euler(targetCharacterDirection);
+
+        // Get raw mouse input for a cleaner reading on more sensitive mice.
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        // Scale input against the sensitivity setting and multiply that against the smoothing value.
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+
+        // Interpolate mouse movement over time to apply smoothing delta.
+        _smoothMouse.x = Mathf.Lerp(_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
+        _smoothMouse.y = Mathf.Lerp(_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
+
+        // Find the absolute mouse movement value from point zero.
+        _mouseAbsolute += _smoothMouse;
+
+        // Clamp and apply the local x value first, so as not to be affected by world transforms.
+        if ((-minimumX + maximumX) < 360f)
+            _mouseAbsolute.x = Mathf.Clamp(_mouseAbsolute.x, minimumX, maximumX);
+
+        // Then clamp and apply the global y value.
+        _mouseAbsolute.y = Mathf.Clamp(_mouseAbsolute.y, minimumY, maximumY);
+
+        transform.localRotation = Quaternion.AngleAxis(-_mouseAbsolute.y, targetOrientation * Vector3.right) * targetOrientation;
+
+        // If there's a character body that acts as a parent to the camera
+        if (characterBody)
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, Vector3.up);
+            characterBody.transform.localRotation = yRotation * targetCharacterOrientation;
+        }
+        else
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, transform.InverseTransformDirection(Vector3.up));
+            transform.localRotation *= yRotation;
+        }
+    }
+}
