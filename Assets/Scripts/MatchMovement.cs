6584889e112f63f9d19294b3ba58c59afc9daf3a
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2019-04-29 22:25:46 +0100

    rig fixed, build v0.2

---
+++ Assets/Scripts/MatchMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MatchMovement : MonoBehaviour
+{
+    public Transform source;
+    public float scale = 5f;
+
+    private Vector3 pos;
+    private Vector3 sourcePos;
+
+    void Start()
+    {
+        if (source != null)
+        {
+            pos = transform.position;
+            sourcePos = source.position;
+        }
+    }
+
+    void Update()
+    {
+        if (source != null)
+        {
+            transform.position = pos + (sourcePos - source.position) * scale;
+        }
+    }
+}
